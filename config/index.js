bd24197a06608adde8a98516b77d6f51a861c388
梅狸猫 <72137328+Loake@users.noreply.github.com>
2024-06-25 09:55:21 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxf59f501de3137364",
+  appsecret: "15d3e027a53a48b51c472c28b1318a38",
+  template_id: "-nTW8tkhHhOW787ETl6ucn-2FbeHDg4vRxcILhzb82c",
+  touser: "o4zbf6Xgvz-AlUQp845iY_8XYoCU",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "襄阳",
+  first_date: "2024-06-18",
 };
