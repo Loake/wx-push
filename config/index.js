a3d56b348e3c4d4e614bc7adb2ad59b3f48fd087
梅狸猫 <72137328+Loake@users.noreply.github.com>
2024-06-30 09:46:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wxf59f501de3137364",
   appsecret: "15d3e027a53a48b51c472c28b1318a38",
   template_id: "-nTW8tkhHhOW787ETl6ucn-2FbeHDg4vRxcILhzb82c",
-  touser: "o4zbf6Xgvz-AlUQp845iY_8XYoCU",
+  touser: "o4zbf6Xgvz-AlUQp845iY_8XYoCU常梦堯的OpenID",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "襄阳",
